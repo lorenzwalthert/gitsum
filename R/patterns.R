f33980165e1a216856634bc8cd76ebc27b0b2a1f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-05-25 18:28:18 +0200

    fix bug for binary files

--- R/patterns.R
+++ R/patterns.R
@@ -4,6 +4,6 @@
 #'   git log.
 get_pattern_multiple <- function() {
   c(all_changes         = "^\\s(\\d+)\\sfiles?\\schanged,?\\s?(\\d+)?\\s?i?n?s?e?r?t?i?o?n?s?\\(?\\+?\\)?,?\\s?(\\d+)?\\s?d?e?l?e?t?i?o?n?s?\\(?\\-?\\)?$",
-    all_changes_file    = "^\\s(.*)\\s+\\|\\s+(\\d+)\\s(\\+*)(\\-*)")
+    all_changes_file    = "^\\s(.*)\\s+\\|\\s*(\\d+)*\\s*(\\+*)(\\-*)")
 }
 
