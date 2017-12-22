87d190e51110263a3d0b64d2774730edadc99acf
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-12-22 23:04:53 +0100

    more skipping

--- tests/testthat/test-report_generation.R
+++ tests/testthat/test-report_generation.R
@@ -1,7 +1,7 @@
 library("ggplot2")
 path_det <- testthat_file("test_logs/testthat_log_detailed.txt")
 test_that("repo reports can be created", {
-  skip_on_appveyor()
+  skip_on_os("windows")
   if (!is_gitsum_repo()) init_gitsum()
   expect_error(git_report(
     path = "/",
