55a93909a60250d67c4ae4dba70e22b600df5c8b
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-12-23 22:37:47 +0100

    documenting

--- R/renamed-files.R
+++ R/renamed-files.R
@@ -1,16 +1,15 @@
 #' Set the column  `changed_files` of a `log` to the latest name of the file
 #'
 #' [parse_log_detailed()] parses a git log. However, changed file names
-#' are not corrected retroactive. Hence, if one want's to process file-specific
+#' are not corrected retroactively. Hence, if one want's to process file-specific
 #' commit data for one file accross different names for that file, the column
 #' `changed_files` has to be unified. This is done with this function by
-#' updating the file names recursively and setting them to the latest name of
-#' all files.
+#' updating the file names recursively and setting them to the latest name used.
 #' @param log An unnested detailed log.
 #' @examples
 #' library("magrittr")
 #' gitsumlog %>%
-#' unnest() %>%
+#'   tidyr::unnest() %>%
 #'   set_changed_file_to_latest_name() %>%
 #'   add_line_history()
 #' @export
@@ -45,7 +44,7 @@ is_name_change <- function(changed_file,
 #' Takes a vector of raw reassignments and turns them into a list of elements.
 #' Every element contains the old path, the new path and the index of the
 #' reassignment.
-#' @param raw_reasignment Character vector with raw re-assignments.
+#' @param raw_reassignment Character vector with raw re-assignments.
 #' @param reassignment_index Integer vector indicating the position of the
 #'   re-assignment in the original unnested detailed log.
 #' @examples
@@ -62,6 +61,7 @@ parse_reassignment <- function(raw_reassignment, reassignment_index) {
 #'
 #' @importFrom purrr map
 #' @importFrom stringr str_split fixed str_sub
+#' @inheritParams parse_reassignment
 #' @examples
 #' gitsum:::separate_dir_and_reassignment("R/{gitsum.R => gitsum-package.R}")
 separate_dir_and_reassignment <- function(raw_reassignment) {
@@ -81,6 +81,7 @@ separate_old_and_new_name <- function(reassignment) {
 #' @param dirs Character vector with directories.
 #' @param base_names List of pairs containing the old base name as the first
 #'   element and the new base name as the second.
+#' @inheritParams parse_reassignment
 #' @examples
 #' gitsum:::combine_dir_and_base("R", list(c("a", "b")), 1)
 #' @importFrom purrr pmap
@@ -91,10 +92,12 @@ combine_dir_and_base <- function(dirs, base_names, reassignment_index) {
 
 #' Combine dir with old and new basename
 #'
-#' Takes a dir and the new
+#' @param dirname A directory name.
+#' @param base_name The base name of a file.
+#' @inheritParams parse_reassignment
 #' @importFrom rlang ll !!!
-combine_dir_and_base_one <- function(dirname, basename, reassignment_index) {
-  out <- ll(!!!as.list(file.path(dirname, basename)), reassignment_index) %>%
+combine_dir_and_base_one <- function(dirname, base_name, reassignment_index) {
+  out <- ll(!!!as.list(file.path(dirname, base_name)), reassignment_index) %>%
     set_names(c("old_path", "new_path", "reassignment_index"))
   out
 }
@@ -106,12 +109,15 @@ combine_dir_and_base_one <- function(dirname, basename, reassignment_index) {
 #' transition scheme provided in `transition_model`.
 #' @param transition_model A transition model obtained from
 #' [parse_reassignment()].
+#' @inheritParams set_changed_file_to_latest_name
 #' @importFrom purrr reduce
 update_changed_file_sequentially <- function(log, transition_model) {
   reduce(transition_model, update_changed_file, .init = log)
 }
 
 #' Apply one file name update to a log
+#' @inheritParams set_changed_file_to_latest_name
+#' @param transition A named list with one entry from the transition model.
 #' @importFrom rlang seq2
 #' @importFrom dplyr bind_rows
 update_changed_file <- function(log, transition) {
@@ -126,6 +132,7 @@ update_changed_file <- function(log, transition) {
 #' Apply one file name update to an unnested log that contains commits
 #' up to that name change. Also needs to update the last entry in log, which
 #' is the transition itself.
+#' @inheritParams update_changed_file
 update_changed_file_one <- function(log, transition) {
   to_update <- log$changed_file %in% transition$old_path
   log[c(which(to_update), nrow(log)), "changed_file"] <- transition$new_path
@@ -134,6 +141,7 @@ update_changed_file_one <- function(log, transition) {
 
 #' Create indices for splitting `log` into a group to update and a group to
 #' not update
+#' @inheritParams update_changed_file_one
 #' @importFrom forcats fct_inorder
 group_reassignment <- function(log, transition) {
   fct_inorder(c(
