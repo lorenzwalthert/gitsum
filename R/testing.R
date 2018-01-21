ae53e28231adbc6d46249c513da5b0daa0aea26b
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-21 17:37:35 +0100

    document

--- R/testing.R
+++ R/testing.R
@@ -7,8 +7,12 @@ testthat_file <- function(...) {
 
 #' Parse a test log
 #'
+#' Function to facilitate the reading and parsing of a raw log for unit test
+#' purposes.
 #' @param path_to_raw_log The path to the raw log relative to tests/testthat/.
 #' @param ... Params passed to [parse_log_detailed_full_run()].
+#' @param parser The parser to use, i.e. either [parse_log_detailed_full_run()]
+#'   or [parse_log_simple()].
 parse_test_log <- function(path_to_raw_log, parser = parse_log_detailed_full_run, ...) {
   path <- testthat_file(path_to_raw_log)
   parser(
